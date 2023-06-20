b89b357cf06902562fb5f94225f97ad14a8f68e5
Mark Carlton <mcarlton33@gmail.com>
2023-06-20 19:25:51 +0300

    track-and-trace swagger specification

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.NODE_ENV || 3000;
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Track-and-Trace')
+    .setDescription('Track-and-Trace API')
+    .setVersion('1.0')
+    .addTag('default')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
